9941beef85d487ced98d1261be85661d89084bd7
hennanlewis <39320310+hennanlewis@users.noreply.github.com>
2023-11-13 10:51:05 -0300

    Initial Flask config

--- txt-recognize-py/app.py
+++ txt-recognize-py/app.py
@@ -1,25 +1,34 @@
 import time
 
-from recognizer import get_all_images_names, get_clipboard_image, text_from_clipboard_image
-from recognizer import execution_end_sound, image_text_from_image_folder
+from text_recognizer import get_all_images_names, get_clipboard_image, text_from_clipboard_image
+from text_recognizer import execution_end_sound, image_text_from_image_folder
 
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder="assets")
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
 
 if __name__ == "__main__":
-	start_time = time.time()
+	# start_time = time.time()
 	
-	clipboard_image = get_clipboard_image()
-	if clipboard_image:
-		text_from_clipboard_image(clipboard_image)
-		execution_end_sound()
+	# clipboard_image = get_clipboard_image()
+	# if clipboard_image:
+	# 	text_from_clipboard_image(clipboard_image)
+	# 	execution_end_sound()
 
-	img_names = get_all_images_names()
-	text = image_text_from_image_folder(img_names)
+	# img_names = get_all_images_names()
+	# text = image_text_from_image_folder(img_names)
 
-	end_time = time.time()
-	total_time = end_time - start_time
-	print(f"Character recognition completed in {round(total_time)} seconds")
+	# end_time = time.time()
+	# total_time = end_time - start_time
+	# print(f"Character recognition completed in {round(total_time)} seconds")
 
-	if text:
-		with open("output_file.txt", "w", encoding="utf-8") as file:
-			file.write(text)
-		print("Recognized text saved in output_file.txt")
+	# if text:
+	# 	with open("output_file.txt", "w", encoding="utf-8") as file:
+	# 		file.write(text)
+	# 	print("Recognized text saved in output_file.txt")
+
+    app.run(debug=True)
