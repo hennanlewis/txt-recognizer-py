273a28a687f92e27539328e6baf189658ec73756
hennanlewis <39320310+hennanlewis@users.noreply.github.com>
2023-11-21 11:51:46 -0300

    refac: all non used functions was removed

--- txt-recognize-py/app.py
+++ txt-recognize-py/app.py
@@ -1,7 +1,6 @@
 import time
 
-from text_recognizer import get_all_images_names, get_clipboard_image, text_from_clipboard_image
-from text_recognizer import execution_end_sound, image_text_from_image_folder
+from text_recognizer import get_clipboard_image, text_from_clipboard_image
 
 from flask import Flask, render_template, jsonify
 
@@ -23,23 +22,4 @@ def clipboard_recognition():
 
 
 if __name__ == "__main__":
-	# start_time = time.time()
-	
-	# clipboard_image = get_clipboard_image()
-	# if clipboard_image:
-	# 	text_from_clipboard_image(clipboard_image)
-	# 	execution_end_sound()
-
-	# img_names = get_all_images_names()
-	# text = image_text_from_image_folder(img_names)
-
-	# end_time = time.time()
-	# total_time = end_time - start_time
-	# print(f"Character recognition completed in {round(total_time)} seconds")
-
-	# if text:
-	# 	with open("output_file.txt", "w", encoding="utf-8") as file:
-	# 		file.write(text)
-	# 	print("Recognized text saved in output_file.txt")
-
 	app.run(debug=True)
