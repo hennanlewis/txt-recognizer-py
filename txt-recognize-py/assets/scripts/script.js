084eb5151f8d37f3e6166b8b1ea26ee5262452c5
hennanlewis <39320310+hennanlewis@users.noreply.github.com>
2023-11-27 05:23:42 -0300

    feat: using promisse to run functions from python

--- txt-recognize-py/assets/scripts/script.js
+++ txt-recognize-py/assets/scripts/script.js
@@ -3,6 +3,7 @@ const clipboardCheckbox = document.querySelector("#clipboard")
 const selectedLanguage = document.querySelector("#language")
 const pathButton = document.querySelector("#path-button")
 const pathInput = document.querySelector("#path-input")
+const result = document.querySelector("#result")
 
 const soundNotification = new Audio("/sound/finished.mp3")
 
@@ -14,11 +15,16 @@ pathButton.addEventListener("click", async () => {
 
 startRecognitionButton.addEventListener("click", async (event) => {
 	event.target.classList.add("active")
-	const pyFuncToExecution = clipboardCheckbox.checked ? "clipboard_recognition" : "local_files_recognition"
-	eel[pyFuncToExecution]()((pyReturn) => {
-		const result = document.querySelector("#result")
-		result.value = pyReturn
-		soundNotification.play()
-		event.target.classList.remove("active")
-	})
+	const pythonFunctionName = clipboardCheckbox.checked ? "clipboard_recognition" : "local_files_recognition"
+	const textRecognized = await pythonFunctionToExecute(pythonFunctionName)
+	result.value = textRecognized
+	soundNotification.play()
+	event.target.classList.remove("active")
 })
+
+const pythonFunctionToExecute = async (functionName) => {
+	return new Promise(resolver => {
+		eel[functionName]()((pyReturn) => resolver(pyReturn))
+	})
+}
+
